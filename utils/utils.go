d02f41ff203adbdd63e7ac709801593e020a00b7
iavl <z6521256@gmail.com>
2020-10-04 21:39:58 +0800

    update log

--- utils/utils.go
+++ utils/utils.go
@@ -184,7 +184,7 @@ func QueryPaillierResult(taskId string) (result *big.Int) {
 
 func PaillerMain(pk *paillier.PublicKey, sk *paillier.PrivateKey, dataList []int64, taskId string) (cipherTextList []string, txHashList []string, encryptResult *big.Int, decryptResult int64, err error) {
 	fmt.Println(fmt.Sprintf("\n\n=========================================================="))
-	fmt.Println(fmt.Sprintf("task id: %s", taskId))
+	fmt.Println(fmt.Sprintf("开始一次新的计算任务，task id: %s", taskId))
 
 	//fmt.Println(fmt.Sprintf("N2: %d", pk.N2))
 	ResetN2(pk.N2, taskId)
